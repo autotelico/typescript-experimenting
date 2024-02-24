4770b9da50be60f624e5dd62771d584ef94ba885
Henrique Heron <henriqueheron1@gmail.com>
2024-02-24 14:44:48 -0300

    testing unknown, void, never, and custom types

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var myName = 'autotelico';
+var myName = "autotelico";
+function handleResponse(response) {
+    switch (response.status) {
+        case "ONLINE":
+            console.log("You are online!");
+            break;
+        case "OFFLINE":
+            console.log("You are offline.");
+            break;
+        case "IDLE":
+            console.log("a");
+            break;
+        default:
+            var status_1 = response.status;
+            throw new Error("Unknown status: " + status_1);
+    }
+}
+handleResponse({ status: "ONLINE" });
--- index.ts
+++ index.ts
@@ -1,3 +1,34 @@
 type Name = string;
 
-const myName: Name = 'autotelico';
+const myName: Name = "autotelico";
+
+interface StreamResponse {
+  status: StreamStatus;
+}
+
+type StreamStatus = "ONLINE" | "OFFLINE" | "IDLE";
+
+function handleResponse(response: StreamResponse): void {
+  switch (response.status) {
+    case "ONLINE":
+      console.log("You are online!");
+      break;
+    case "OFFLINE":
+      console.log("You are offline.");
+      break;
+    case "IDLE":
+      console.log("a");
+      break;
+    default:
+      const status: never = response.status;
+      throw new Error("Unknown status: " + status);
+  }
+}
+
+handleResponse({ status: "ONLINE" });
+
+function logSomething(value: string): void {
+  console.log(value);
+}
+
+console.log(logSomething("hey"));
