157c48e865eeb7c0f6f8f88e26e6cbfe3d68b24e
Henrique Heron <henriqueheron1@gmail.com>
2024-02-24 13:11:29 -0300

    funny update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
 fs.writeFile('README.md', 'A repo for me to push some TypeScript experiments and practice stuff.', err => {
-    if (err) throw new Error('Couldn\'t write file.');
+    if (err) throw new Error('Couldn\'t write file cuzzzzz idk lol this wasn\'t supposed to happen.');
 })
